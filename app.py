090d51badbd81097d3588a8ed2012170acc34b6d
Mario Agustín Sgró <observandum@gmail.com>
2021-05-17 15:37:20 -0300

    update

--- app.py
+++ app.py
@@ -7,25 +7,38 @@ import numpy as np
 from astropy.table import Table
 from joblib import load
 import base64
+import plotly.express as px
+import plotly.graph_objects as go
  
 st.title('VVVx Classification')
-st.header("Random Forest clasifier")
-st.subheader("Params")
+st.header("Random Forest classifier")
+#st.subheader("Params")
 #st.text("Esto es texto")
 #st.latex("y = x^2")
 #st.code("if a == 1:\n    print(a)", language="python")
 #st.code("var a = 1;", language="javascript")
 #st.markdown("Esto es **texto** usando *Markdown*")
 
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
+def download_link(object_to_download, download_filename, download_link_text):
     """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-    return(href)
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 def proccess_fits(_data):
   names = [name for name in _data.colnames if len(_data[name].shape) <= 1]
@@ -56,11 +69,11 @@ columns = ['MAG_PSF','MAG_AUTO','MAG_APER','MAG_MODEL','SPREAD_MODEL',
 with open(f'pipeline.joblib', 'rb') as f:
     model = load(f)
 
-filetype = st.radio("What's your favorite movie genre",('Fits', 'CSV'))
+filetype = st.radio("File format",('Fits', 'CSV'))
 
-test = "False"
-if filetype == "CSV":
-  test = st.radio("Realizar el test",("False","True"))
+confusion_matrix = st.radio("Compute confusion matrix",(False,True))
+
+return_probability = st.radio("Return probability",(False,True))
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -71,13 +84,13 @@ if uploaded_file is not None:
       data = pd.read_csv(uploaded_file,delim_whitespace=True)
       data = proccess_csv(data)
 
-    if test == "True": 
+    if confusion_matrix:
       if "GALAXY" not in data.columns:
         st.error('Dataframe must contain boolean column named "GALAXY"')
         st.stop()
       else:
         y = data['GALAXY']
-
+      
     data['colorJH'] = data['MAG_APER_J_CORR'] - data['MAG_APER_H_CORR']
     data['colorHKs'] = data['MAG_APER_H_CORR'] - data['MAG_APER_KS_CORR']
     data['colorJKs'] = data['MAG_APER_J_CORR'] - data['MAG_APER_KS_CORR']
@@ -88,19 +101,43 @@ if uploaded_file is not None:
         st.stop()
         
     X = data[columns]
+
     predictions = model.predict(X)
-    
     ngals = sum(predictions == 1)
     nothers = sum(predictions == 0)
-
-    st.markdown("10 random-rows")
-    st.dataframe(X.sample(10))
+    
+    st.markdown("Dataframe")
+    st.dataframe(X.describe())
 
     st.header("Results")
-    st.markdown("Ngals: %d" % ngals)
-    st.markdown("Nothers: %d" % nothers)
+    st.markdown("Galaxy objects: %d" % ngals)
+    st.markdown("No-galaxy objects: %d" % nothers)
 
-    if test == "True":
+    fig = go.Figure()
+    indx = predictions == 1
+    fig.add_trace(go.Scatter(x=X["MAG_AUTO"][indx],y=X["colorJKs"][indx],
+                             mode='markers',
+                             hovertemplate =
+                             '<b>Galaxy</b>'+
+                             '<br><b>MAG_AUTO_Ks</b>: %{x:.2f}'+
+                             '<br><b>Color J-Ks</i>: %{y:.2f}',
+                             name='galaxy'))
+
+    indx = predictions == 0
+    fig.add_trace(go.Scatter(x=X["MAG_AUTO"][indx],y=X["colorJKs"][indx],
+                             mode='markers',
+                             hovertemplate =
+                             '<b>No-Galaxy</b>'+
+                             '<br><b>MAG_AUTO_Ks</b>: %{x:.2f}'+
+                             '<br><b>Color J-Ks</i>: %{y:.2f}',
+                             name='no-galaxy'))
+
+    fig.update_layout(height=600)
+    fig.update_yaxes(title='color J-Ks')
+    fig.update_xaxes(title='MAG_AUTO_Ks')
+    st.write(fig)
+
+    if confusion_matrix:
       np.set_printoptions(precision=2)
 
       y_true, y_pred = y, predictions
@@ -127,4 +164,8 @@ if uploaded_file is not None:
 
       st.pyplot(fig)
 
-      st.markdown(get_table_download_link(pd.DataFrame(predictions)), unsafe_allow_html=True)
+    st.markdown(download_link(pd.DataFrame(predictions),'class.dat','Click here to download classification'), unsafe_allow_html=True)
+      
+    if return_probability:
+      probabilities = model.predict_proba(X)
+      st.markdown(download_link(pd.DataFrame(probabilities[:,1]),'prob.dat','Click here to download propabilities'), unsafe_allow_html=True)
